20d9fc46706484edfd9efc2dcd0ff770d08ea679
Jeremy Howard <j@fast.ai>
2020-08-11 14:32:42 -0700

    output-folder

--- create-gh-yaml.sh
+++ create-gh-yaml.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-CONDA_PREFIX="${CONDA_PREFIX:-/opt/conda}"
 python create-gh-yaml.py
-conda build gh
-conda convert $CONDA_PREFIX/conda-bld/linux-64/gh*.tar.bz2 -p linux-32 -p osx-64 -o build
-anaconda -t $TOKEN upload --skip $CONDA_PREFIX/conda-bld/linux-64/gh*.tar.bz2
+conda build gh --output-folder build
+conda convert build/linux-64/gh*.tar.bz2 -p linux-32 -p osx-64 -o build
 anaconda -t $TOKEN upload --skip build/*/*.tar.bz2
 
