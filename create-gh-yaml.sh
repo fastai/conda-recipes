e9e19350b1830b3c2f343a56479f4fe46fc396e3
Jeremy Howard <j@fast.ai>
2020-08-11 13:31:54 -0700

    prefix

--- create-gh-yaml.sh
+++ create-gh-yaml.sh
@@ -2,6 +2,6 @@
 
 python create-gh-yaml.py
 conda build gh
-conda convert ~/anaconda3/conda-bld/linux-64/gh*.tar.bz2 -p linux-32 -p osx-64 -o build
+conda convert $CONDA_PREFIX/conda-bld/linux-64/gh*.tar.bz2 -p linux-32 -p osx-64 -o build
 anaconda -t $TOKEN upload build/*/*.tar.bz2
 
