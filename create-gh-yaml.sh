537dd9ba8284f7446e6fc2e96663595e1a810ecf
Jeremy Howard <j@fast.ai>
2020-08-11 13:57:10 -0700

    skip

--- create-gh-yaml.sh
+++ create-gh-yaml.sh
@@ -4,5 +4,6 @@ CONDA_PREFIX="${CONDA_PREFIX:-/opt/conda}"
 python create-gh-yaml.py
 conda build gh
 conda convert $CONDA_PREFIX/conda-bld/linux-64/gh*.tar.bz2 -p linux-32 -p osx-64 -o build
-anaconda -t $TOKEN upload build/*/*.tar.bz2
+anaconda -t $TOKEN upload --skip $CONDA_PREFIX/conda-bld/linux-64/gh*.tar.bz2
+anaconda -t $TOKEN upload --skip build/*/*.tar.bz2
 
